0e8c3ff877e2a0ae1dffe2f5a466a692587a922f
Your Name <your@email.com>
2026-09-11 21:24:21 +0500

    Draw detection logic , Win detection logic in Tic Tac Toe

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def checkwin(xState, zState):
         if(sum(xState[win[0]],xState[win[1]],xState[win[2]]) == 3):
             print("X wins")
             return 1
+          
         if(sum(zState[win[0]],zState[win[1]],zState[win[2]]) == 3):
             print("O wins!")
             return 0
+            
     return -1
 
 if __name__ == "__main__":
@@ -34,18 +36,47 @@ if __name__ == "__main__":
     turn = 1 # 1 for x and 0 for O
     print("welcome to tic tac toe ")
     while(True):
+        sumup = 0
+        print(xState, zState)
         printBoard(xState,zState)
         if(turn == 1):
             print("X's Chance")
             value = int(input("please enter a value: "))
+            if(value in range(0,9)):
+                if(zState[value] == 1 or xState[value] == 1):
+                    print("place already filled, please try again")
+                    continue    
+            else:
+                print("Invalid input, please enter a value between 0 and 8")
+                continue
             xState[value] = 1
         else:
             print("O's Chance")
             value = int(input("please enter a value: "))
-            zState[value] = 1   
-     
+            if(value in range(0,9)):
+                if(xState[value] == 1 or zState[value] == 1):
+                    print("place already filled, please try again")
+                    continue
+                zState[value] = 1
+            else:
+                print("Invalid input, please enter a value between 0 and 8")
+                continue
+      
         cwin = checkwin(xState,zState)
         if(cwin != -1):
             print("game over")
+            printBoard(xState, zState)
             break
+        else:
+            for x in range(0,9):
+                    sumup = xState[x] + zState[x] + sumup
+            if(sumup == 9):
+                     print("its a draw")
+                     printBoard(xState, zState)
+                     break
+
+                  
+
+
+ 
         turn = 1 - turn
