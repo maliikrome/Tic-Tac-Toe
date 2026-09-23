2d3b81defe02bfd088727b19c02de823a5950c8d
Your Name <your@email.com>
2026-09-23 16:47:39 +0500

    Tic Tac Toe Game , using python custotkinter

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-class tic_tac_toe(customtkinter.CTk):
+import customtkinter
+class ToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(self, master, message, Restart):
+        super().__init__(master)
+        self.geometry("400x300")
+        self.message = message
+
+        self.label = customtkinter.CTkLabel(self, text=message)
+        self.label.pack(padx=20, pady=20)
+
+        self.RestartButton = customtkinter.CTkButton(
+                self,
+                text="Restart",
+                width=150, height=40,
+                font=("Arial", 16, "bold"),
+                command=Restart
+            )
+        self.RestartButton.pack(pady=15)
+        self.QuitButton = customtkinter.CTkButton(self, text = "Quit",width=150, height=40,
+                        font=("Arial", 16, "bold"),command = self.Quit)
+        self.QuitButton.pack(pady=15)
+    def Quit(self):
+        app.destroy()
+
+class TicTacToe(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-                
+        self.buttons = [] 
         self.title("Tic Tac Toe")
         self.geometry("500x600")
         self.xState = [0] * 9
         self.zState = [0] * 9
         self.turn = 1
         self.game_over = False
+        self.toplevel_window = None
 
         frame1 = customtkinter.CTkFrame(self)
         frame1.pack(pady=10)
@@ -22,73 +47,25 @@ class tic_tac_toe(customtkinter.CTk):
                                                     font=("Arial", 18))
         self.statusLabel.pack(pady=5)
 
-        self.button0 = customtkinter.CTkButton(frame2, text="0", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(0))
-        self.button0.grid(row=0, column=0)
+        for i in range(0,9):
 
-        self.button1 = customtkinter.CTkButton(frame2, text="1", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(1))
-        self.button1.grid(row=0, column=1)
-
-        self.button2 = customtkinter.CTkButton(frame2, text="2", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(2))
-        self.button2.grid(row=0, column=2)
-
-        self.button3 = customtkinter.CTkButton(frame2, text="3", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(3))
-        self.button3.grid(row=1, column=0)
-
-        self.button4 = customtkinter.CTkButton(frame2, text="4", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(4))
-        self.button4.grid(row=1, column=1)
-
-        self.button5 = customtkinter.CTkButton(frame2, text="5", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(5))
-        self.button5.grid(row=1, column=2)
-
-        self.button6 = customtkinter.CTkButton(frame2, text="6", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(6))
-        self.button6.grid(row=2, column=0)
-
-        self.button7 = customtkinter.CTkButton(frame2, text="7", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(7))
-        self.button7.grid(row=2, column=1)
-
-        self.button8 = customtkinter.CTkButton(frame2, text="8", width=100, height=100,
-                                               font=("Arial", 30, "bold"),
-                                               text_color_disabled="white",
-                                               command=lambda: self.buttonclicked(8))
-        self.button8.grid(row=2, column=2)
-
-        self.buttons = [self.button0, self.button1, self.button2,
-                        self.button3, self.button4, self.button5,
-                        self.button6, self.button7, self.button8]
-        self.restartButton = customtkinter.CTkButton(
+            b = customtkinter.CTkButton(frame2, text=i, width=100, height=100,
+                                                font=("Arial", 30, "bold"),
+                                                text_color_disabled="white",
+                                                command=lambda idx = i: self.ButtonClicked(idx))
+            b.grid(row=i//3, column=i%3)
+            self.buttons.append(b)
+     
+        self.RestartButton = customtkinter.CTkButton(
             self,
             text="Restart",
             width=150, height=40,
             font=("Arial", 16, "bold"),
-            command=self.restart,
+            command=self.Restart,
         )
-        self.restartButton.pack(pady=15)
+        self.RestartButton.pack(pady=15)
 
-    def checkwin(self):
+    def CheckWin(self):
             wins = [[0,1,2],[3,4,5],[6,7,8],
                 [0,3,6],[1,4,7],[2,5,8],
                 [0,4,8],[2,4,6]]
@@ -100,47 +77,67 @@ class tic_tac_toe(customtkinter.CTk):
             if sum(self.xState) + sum(self.zState) == 9:
              return "Draw"
             return None
-    def buttonclicked(self, index):
-            if self.game_over:
-                return
-            if self.xState[index] or self.zState[index]:
-                return
+    def ButtonClicked(self, index):
+        if self.game_over:
+            return
+        if self.xState[index] or self.zState[index]:
+            return
 
-            if self.turn == 1:
-                self.xState[index] = 1
-                self.buttons[index].configure(text="X", fg_color="dark blue",
-                                            state="disabled")
-                self.turn = 0
-            else:
-                self.zState[index] = 1
-                self.buttons[index].configure(text="O", fg_color="dark blue",
-                                            state="disabled")
-                self.turn = 1
+        if self.turn == 1:
+            self.xState[index] = 1
+            self.buttons[index].configure(text="X", fg_color="dark blue", state="disabled")
+            self.turn = 0
+        else:
+            self.zState[index] = 1
+            self.buttons[index].configure(text="O", fg_color="dark blue", state="disabled")
+            self.turn = 1
 
-            result = self.checkwin()
+        result = self.CheckWin()          
 
-            if result in ("X", "O", "Draw"):
-                self.game_over = True
-                for b in self.buttons:
-                    b.configure(state="disabled")
-                if result == "Draw":
-                    self.statusLabel.configure(text="It's a draw!")
+        if result in ("X", "O", "Draw"):
+            self.game_over = True
+            for b in self.buttons:
+                b.configure( fg_color = '#0a172b')
+                if b.cget("state") == "normal":
+                    b.configure(state="disabled",)
+            if result == "Draw":
+                message = 'DRAW'
+                self.statusLabel.configure(text="It's a draw!", font=("Arial", 30, "bold"))
+
+                if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+                  
+                   self.toplevel_window = ToplevelWindow(self,message,self.Restart)  # create window if its None or destroyed
                 else:
-                    self.statusLabel.configure(text=f"{result} wins!")
+                  
+                   self.toplevel_window.focus()  # if window exists focus it
             else:
-                who = "X's" if self.turn == 1 else "O's"
-                self.statusLabel.configure(text=f"{who} Turn")
+                message = f"{result} wins!"
+                self.statusLabel.configure(text=f"{result} wins!", font=("Arial", 30, "bold"))
 
-    def restart(self):
+                if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+                 
+                   self.toplevel_window = ToplevelWindow(self,message, self.Restart)  # create window if its None or destroyed
+
+                else:
+                 
+                   self.toplevel_window.focus()  # if window exists focus it
+        else:
+            who = "X" if self.turn == 1 else "O"
+            self.statusLabel.configure(text=f"{who}'s Turn")   # <-- no self.frame1
+    def Restart(self):
             self.xState = [0,0,0,0,0,0,0,0,0]
             self.zState = [0,0,0,0,0,0,0,0,0]
             self.turn = 1
             self.game_over = False
 
-            for i, b in enumerate(self.buttons):
-                b.configure(text=str(i), state="normal",
-                            fg_color=("#3B8ED0", "#1F6AA5"))
+            if self.toplevel_window is not None and self.toplevel_window.winfo_exists():
+               self.toplevel_window.destroy()
+               self.toplevel_window = None
+
+            for i, button in enumerate(self.buttons):
+              button.configure(text=str(i), state="normal", fg_color=("#3B8ED0", "#1F6AA5"))
 
             self.statusLabel.configure(text="Turn: X")
-app = tic_tac_toe()
+
+app = TicTacToe()
 app.mainloop()
